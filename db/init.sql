64bfa22aacd80679421f86c0be661a0511131f03
Yusup Bagamedov <it0005@ya.ru>
2024-10-25 23:33:06 +0300

    psql_table4

--- db/init.sql
+++ db/init.sql
@@ -1,8 +1,8 @@
-echo "CREATE TABLE IF NOT EXISTS keys (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
-    key_value VARCHAR(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS keys (
+                                    id SERIAL PRIMARY KEY,
+                                    user_id INTEGER NOT NULL,
+                                    key_value VARCHAR(255) NOT NULL,
     creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+    );
 
-INSERT INTO keys (user_id, key_value) VALUES (1, 'sample_key');" > init.sql
+INSERT INTO keys (user_id, key_value) VALUES (1, 'sample_key');

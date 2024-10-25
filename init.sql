a006bd679676f618960e6ddc5840dffe63fb42a1
Yusup Bagamedov <it0005@ya.ru>
2024-10-25 22:57:40 +0300

    psql_table

---
+++ init.sql
@@ -0,0 +1,9 @@
+-- init.sql
+CREATE TABLE IF NOT EXISTS keys (
+                                    id SERIAL PRIMARY KEY,
+                                    user_id INTEGER NOT NULL,
+                                    key_value VARCHAR(255) NOT NULL,
+    creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+INSERT INTO keys (user_id, key_value) VALUES (1, 'sample_key');
